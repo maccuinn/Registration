20ada3884a9f971209069ee759f39e30e1612eed
maccuinn <april.l.hall@gmail.com>
2018-06-19 10:45:38 -0700

    added view controllers to views

---
+++ Registration/Controllers/NewHandlerViewController.swift
@@ -0,0 +1,35 @@
+//
+//  NewHandlerViewController.swift
+//  Registration
+//
+//  Created by April Hall on 2018-06-19.
+//  Copyright © 2018 April Hall. All rights reserved.
+//
+
+import UIKit
+
+class NewHandlerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
